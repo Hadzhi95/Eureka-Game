1b259672d6e9cd553a4ca21c3a9d683f870057f5
Hadzhi95 <kreyg1992@bk.ru>
2022-03-20 01:19:25 +0300

    flippingAnimation

--- magic-memory/src/App.js
+++ magic-memory/src/App.js
@@ -46,8 +46,7 @@ function App() {
         })
         resetTurn()
       } else {
-        
-        resetTurn()
+        setTimeout(() => resetTurn(), 1000)
       }
     }
   }, [choiceOne, choiceTwo]);
@@ -70,6 +69,7 @@ function App() {
           key={card.id} 
           card={card}
           handleChoice={handleChoice}
+          flipped={card === choiceOne || card === choiceTwo || card.matched}
           />
         ))}
       </div>
